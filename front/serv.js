790bfb55fedd1b63e3029c6b3c88dd03b1434536
xLeDocteurx <lenoirc.fr@gmail.com>
2019-06-24 08:57:11 +0200

    corrected the scripts

--- front/serv.js
+++ front/serv.js
@@ -5,7 +5,8 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    console.log('process.env', process.env)
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(80)
