a5e3fc0c173500f3c04935c59bd1dfcc9aad9029
xLeDocteurx <lenoirc.fr@gmail.com>
2019-06-24 09:15:07 +0200

    corrected the scripts

--- front/serv.js
+++ front/serv.js
@@ -5,8 +5,10 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', function(req, res) {
+    console.log('touch on "/" path')
     console.log('process.env', process.env)
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(80)
+console.log('server started')
